79576e74045706dfd5a7899c82e66009ff300ab3
JoshColyn <129877313+JoshColyn@users.noreply.github.com>
2023-06-21 11:03:37 +0800

    Added fonts to _layout.js

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, Flatlist } from 'react-native';
 
-import styles from './welcome.style'
+import { useRouter } from 'expo-router';
+import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants';
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Joshua</Text>
+        <Text style={styles.userName}>Find a job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+            />
+        </View>
+      </View>
     </View>
   )
 }
